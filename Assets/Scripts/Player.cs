301a3d1216ebee965cc468872dbc4f9b3e85989c
Natalia DaLomba <nataliadalomba@protonmail.com>
2023-04-25 10:05:18 -0400

    Added player speed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float speed = 3.5f;
+    void Start() {
+        //take the current position = new position (0, 0, 0)
+        transform.position = Vector3.zero;
+    }
+
+    void Update() {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
