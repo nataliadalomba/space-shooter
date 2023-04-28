ca61b0631bb837a729a355d2de6fa2bee309f683
Natalia DaLomba <nataliadalomba@protonmail.com>
2023-04-28 10:11:11 -0400

    Enemy spawn, translation and spawns at random location if hits bottom of screen. If player hits enemy, enemy is destroyed. If laser hits enemy, laser and enemy are destroyed

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed = 4f;
+    [SerializeField]
+    private GameObject _player;
+
+    //we have an Enemy prefab in hierachy and we're reusing it
+    //so we don't need to instantiate or destroy it
+
+    void Update() {
+        //move down at 4 meters per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        //if bottom of screen
+        //respawn at top with a new random x position
+        if (transform.position.y <= -5f) {
+            float randX = Random.Range(-12f, 12f);
+            transform.position = new Vector3(randX, 7, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        //if other is player
+        //damage the player (eventually)
+        //destroy us
+        if (other.gameObject == GameObject.FindWithTag("Player")) {
+
+            Destroy(this.gameObject);
+        }
+
+        //if other is laser
+        //destroy laser
+        //destroy us
+        if (other.gameObject == GameObject.FindWithTag("Laser")) {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
